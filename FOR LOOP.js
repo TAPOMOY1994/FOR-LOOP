d89d9e2d4c1f046f5a13152f1df69727817e8155
Tapomoy Das <160303114+TAPOMOY1994@users.noreply.github.com>
2024-04-01 11:54:27 +0530

    Add files via upload

---
+++ FOR LOOP.js
@@ -0,0 +1,23 @@
+
+
+// Write a program to print numbers from 1 to 10//
+
+
+
+
+for(let i=1;i<=10;i++)
+{
+    console.log(i);
+}
+
+
+
+// Write a program to print odd numbers from 49 to 1//
+
+
+
+for(let i=49;i>=1;i-=2)
+{
+
+    console.log(i);
+}
